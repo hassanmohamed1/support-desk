8e4ed6fe96b602195857505120beb89869239eca
Hassan Mohamed <hassanmohamed9402@gmail.com>
2022-05-09 17:22:52 -0500

    support-desk

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const colors = require("colors")
+const dotenv = require("dotenv").config()
+const {errorHandler} = require("./middleware/errorMidlleware")
+const connectDB = require("./config/db")
+const PORT = process.env.PORT || 5000
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Welcome to the Support API"})
+})
+
+app.use("/api/users", require("./routes/useRoutes"))
+app.use("/api/tickets", require("./routes/ticketRoutes"))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
